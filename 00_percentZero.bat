641531a0c41428355e508b5371f1e85e5c863792
Arafat Sarkar <arafatgovt01@gmail.com>
2025-05-11 13:06:04 +0600

    SECOND commit

---
+++ 00_percentZero.bat
@@ -0,0 +1,7 @@
+::This is the introduction of the Batch scripting or command line scripting
+::Script is a set of instructions that are meant to be executed in a serial.
+::Batch is a scripting language that is meant to automate tasks for the windows operating system
+::The files have extension of .bat or .cmd
+echo Don't be afraid of learning new things though that terrifies you
+echo The name of the file is %0
+::The %0 is the script path of the script you're writing
